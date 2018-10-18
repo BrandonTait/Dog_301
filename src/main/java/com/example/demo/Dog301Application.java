196f5ad426e7d925c15ddf48af7077dd360c96e2
Brandon <mrbrandontait@gmail.com>
2018-10-17 22:46:33 -0400

    3.01

---
+++ src/main/java/com/example/demo/Dog301Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dog301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dog301Application.class, args);
+    }
+}
